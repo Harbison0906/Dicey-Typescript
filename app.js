09d471a1407961e38427f99834cc160bba7c88ef
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-11-23 17:23:25 -0600

    created class called Die

---
+++ app.js
@@ -0,0 +1,11 @@
+dieValues = [1, 2, 3, 4, 5, 6];
+
+class Die {
+  constructor(value) {
+    this.value = value;
+  }
+  roll() {
+  let randomVal = dieValues[Math.floor(Math.random()*dieValues.length)]
+  console.log(randomVal);
+  }
+}
