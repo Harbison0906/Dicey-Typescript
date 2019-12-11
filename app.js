0287362dc83d3447ec75d1168df886fe5503128f
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-12-10 21:54:02 -0600

    styling; figured out how to generate a new die with a random die value on a button click

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-let dieVal = [1, 2, 3, 4, 5, 6];
 
 class Die {
   constructor(value) {
-   this.div = $('<div></div>');
-   this.div.attr('id', 'die');
-   this.div.roll();
-   $(this.div).append(randomVal);
-   $('#diceBoard').append(this.div);
-   console.log(this.div);
+    this.div = $('<div></div>');
+    this.div.attr('id', 'die');
+    this.roll();
   }
   roll() {
-    let randomVal = Math.floor(Math.random() * this.dieVal) + 1;
-    console.log(randomVal);
+    let randomVal = Math.floor((Math.random() * 6) + 1);
+    $(this.div).append(randomVal);
+    $('#diceBoard').append(this.div);
   }
 }
 let diceBoard = $('#diceBoard')
 $('body').append(diceBoard);
-$('#gen').click(function() {
+$('#gen').click(function () {
   new Die();
 })
