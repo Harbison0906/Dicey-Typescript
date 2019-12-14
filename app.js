89d7073ccb643ac8c348670a972cbbfa607af02d
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-12-14 15:00:16 -0600

    coded function to re-roll the dice that are currently on the board for new values

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
+const diceArray = [];
 
 class Die {
   constructor(value) {
     this.div = $('<div></div>');
-    this.div.attr('id', 'die');
+    this.div.addClass('die');
     this.roll();
+    $('#diceBoard').append(this.div);
+
   }
   roll() {
     let randomVal = Math.floor((Math.random() * 6) + 1);
     $(this.div).append(randomVal);
-    $('#diceBoard').append(this.div);
+    this.div.attr('id', randomVal);    
   }
+
+  
 }
+
+
 let diceBoard = $('#diceBoard')
 $('body').append(diceBoard);
+
 $('#gen').click(function () {
-  new Die();
+  let newDie = new Die();
+  diceArray.push(newDie);
+  // console.log(diceArray);
+})
+
+$('#roll').click(function () {
+  $('.die').empty();
+  for (i = 0; i < diceArray.length; i++) {
+    diceArray[i].roll();
+  }
+})
+
+diceSum = function(total, num) {
+    return total + num;
+  }
+
+  total = function() {
+    diceArray.reduce(diceSum, 0);
+
+  }
+
+$('#sum').click(function () {
+  sum = 0
+  for (i = 0; i < diceArray.length; i++) {
+    sum += diceArray[i];
+  }
+  console.log(diceArray);
 })
